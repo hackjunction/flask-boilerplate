2cd2048561fb03133a21d282512130d4e997ea54
“tatu <koivisto.tatu@gmail.com>
2017-10-31 22:36:38 +0200

    text areas and other stuff

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField, SelectMultipleField, IntegerField
+from wtforms import TextField, PasswordField, SelectMultipleField, IntegerField, TextAreaField, RadioField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 # Multiselect field without validation
@@ -7,7 +7,6 @@ class NoValidationSelectMultipleField(SelectMultipleField):
     def pre_validate(self, form):
         """per_validation is disabled"""
         return True
-
 
 class CompanyForm(Form):
     name = TextField(
@@ -21,12 +20,25 @@ class CompanyForm(Form):
     contact_email = TextField(
         'Contact Email', validators=[DataRequired(), Length(min=6, max=40)]
     )
+
     excellent_skills = NoValidationSelectMultipleField('Skills applicant should excel at')
     extra_skills = NoValidationSelectMultipleField('Skills applicant should excel at')
     titles = NoValidationSelectMultipleField('Job titles you are hiring for')
 
-    amount_meetings = IntegerField('How many meetings with applicants do you wish to have?')
-    personnel_present = IntegerField('How many meetings with applicants do you wish to have?')
+    amount_meetings = IntegerField('Amoun of meetings')
+    personnel_present = IntegerField('Amount of persons')
+
+    company_strengths = TextAreaField('Company Stregths', validators=[DataRequired(), Length(min=6, max=200)])
+    job_description = TextAreaField('Job Description', validators=[DataRequired(), Length(min=6, max=200)])
+    skills_description = TextAreaField('Contact Email', validators=[DataRequired(), Length(min=6, max=200)])
+
+    job_nature = RadioField('Job nature?', validators=[DataRequired(), Length(min=6, max=200)],
+    choices=[('value_one','Full time'),('value_two','Part Time'),('value_three','Both')])
+
+    experience_in_years = RadioField('Experience', 
+    validators=[DataRequired(), Length(min=6, max=200)],
+    choices=[('value_one','1'),('value_two','1-2 years'),('value_three','3-5 years'),('value_four','5+ years')])
+    
 
     #applicant_properties = NoValidationSelectMultipleField('Skills applicant should excel at')
 
